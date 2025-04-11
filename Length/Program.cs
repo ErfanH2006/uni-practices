bf4ee2e904d1fa8138ab5aa71d39b3bd68a12232
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-11 21:14:17 +0330

    Length of numbers Update

---
+++ Length/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int length = num.ToString().Length;
+
+            Console.WriteLine("Length :" + length);
+        }
+    }
+}
