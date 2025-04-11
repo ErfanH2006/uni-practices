14eb177ace19d6149e9ada1ea0d986b551467197
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-11 21:40:11 +0330

    Reversed number

---
+++ Reverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            string num = Console.ReadLine();
+
+            char[] reversedArray = num.ToCharArray();
+            Array.Reverse(reversedArray);
+
+            string reversedNumber = new string(reversedArray);
+            Console.WriteLine("reversed number : "+ reversedNumber );
+        }
+    }
+}
