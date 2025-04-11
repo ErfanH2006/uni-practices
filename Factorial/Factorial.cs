c352e123ac4723c34d828b87ac7fab171b7b2025
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-11 20:38:42 +0330

    Factorial has added

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number :");
+
+            int number = int.Parse(Console.ReadLine());
+
+            int Factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                Factorial *= i;
+            }
+            Console.WriteLine(Factorial);
+        }
+    }
+}
