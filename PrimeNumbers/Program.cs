44891dfe0ca91f05b05518e1ab95a053553c096f
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-16 13:39:01 +0330

    PrimeNumber

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it is prime:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+
+            if (number < 2)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
+        }
+    }
+}
