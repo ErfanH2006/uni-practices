7f50e47b84bf8ae303708d7d4d8c27a1dddbfd3f
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-16 14:53:14 +0330

    Symmetric Numbers has completed and added to the repository

---
+++ SymmetricNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("all symmetric numbers from 1000 to 9999 are : ");
+            for (int i = 1000; i < 10000; i++)
+            {
+                int a = i / 1000;
+                int b = (i / 100) % 10;
+                int c = (i / 10) % 10;
+                int d = i % 10;
+
+                if (a == d && b == c)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
