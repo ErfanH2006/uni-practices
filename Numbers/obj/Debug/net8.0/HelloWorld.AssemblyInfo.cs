035815df877a6a0d5c10ac83523d6bb332808dc2
ErfanH2006 <erfanhabibi258888@gmail.com>
2025-04-16 15:05:44 +0330

    just restart some extensions

--- Numbers/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Numbers/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14eb177ace19d6149e9ada1ea0d986b551467197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1154b6872645d8587730df77abe2a9f62a47f47b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
